4b9637171c7adedb76383ce05ce91e0bbcdb126a
shopceramic <75972538+shopceramic@users.noreply.github.com>
2020-12-14 14:13:41 +0300

    Update all.js

--- js/all.js
+++ js/all.js
@@ -9,24 +9,26 @@ head_toggler.onclick = function(){
 }
 let head = document.querySelector('header'),
 scrollOn =()=>{
-  window.removeEventListener('scroll',scrollOn)
-  if(innerWidth < 992 && head.getElementsByClassName('d-flex').length > 0) head_toggler.onclick()
-  let h1 = document.querySelector('h1'); h1.style.marginTop = h1.offsetTop
+  window.removeEventListener('scroll',scrollOn);
+  if(innerWidth < 992 && head.getElementsByClassName('d-flex').length > 0) head_toggler.onclick();
+  let h1 = document.querySelector('h1'); h1.style.marginTop = h1.offsetTop;
   head.style.cssText = 'position:fixed;top:0;right:15px;left:15px;z-index:1';
+  let s = document.createElement('script'); s.type = 'text/javascript';
+  s.async = true; s.src = '//code-ya.jivosite.com/widget/ZCCoipNRaL';
+  document.head.append(s);
 };
 window.addEventListener('scroll',scrollOn)
-if(sessionStorage.cart) F.cartHead(JSON.parse(sessionStorage.cart))
+if(sessionStorage.cart) F.cartHead(JSON.parse(sessionStorage.cart));
 for(let btn of head.getElementsByClassName('btn')){
   if(location.pathname == btn.pathname) return btn.classList.add('active')
 }
+(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(70463374, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true });
 
 })
 const F ={
   ajax:(page,fun)=>{
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', '/'+page);
-    xhr.send();
-    xhr.onload = fun;
+    let xhr = new XMLHttpRequest(); xhr.open('GET', '/'+page);
+    xhr.send(); xhr.onload = fun;
   },
   btn:(btn)=>{
     btn.classList.add('pulsate','duration-1500');
