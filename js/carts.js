cf5077cdcbbcd4b61c9dcc7aa18d9cefab272628
shopceramic <75972538+shopceramic@users.noreply.github.com>
2020-12-16 21:25:49 +0300

    Update carts.js

--- js/carts.js
+++ js/carts.js
@@ -49,22 +49,18 @@ checkTel =()=>{
 events();
 document.querySelectorAll('main a').forEach(a => a.onclick =()=>{ window.open(a.href); return false })
 contin.onclick =()=>{
+  alert(document.scripts.length)
   if(document.scripts.length == 3) F.script('//code-ya.jivosite.com/widget/ZCCoipNRaL');
   main.insertAdjacentHTML('beforeEnd', [["Контактное лицо","<input id='nam'","30'/>"],["Конт. номер телефона","<input type='tel' id='tel'","16'/>"],["Адрес доставки","<textarea id='address' rows='4'","300'></textarea>"],["Комментарий","<textarea id='comment' rows='4'","300' placeholder='Необязательное поле'></textarea>"]].reduce((sum,ar)=> `${sum}<div class='col-sm-6 p-4 shadow-sm'><h6 class='alert alert-info'>${ar[0]}</h6>${ar[1]} class='form-control' maxlength='${ar[2]}</div>`,"<div class='row'>")+'</div>')
   contin.textContent = 'Заказать';
   if('onkeyup' in window){ tel.onfocus = checkTel; tel.onblur =()=>{ if(tel.value == '+7(') tel.value = '' } }
   contin.onclick =()=>{
-    let apiResult = jivo_api.sendOfflineMessage({
-      "name": "John Smith",
-      "email": "email@example.com",
-      "phone": "+14084987855",
-      "description": "Description text",
-      "message": "Offline message"
-    });
-    if (apiResult.result === 'ok') {
-        alert('Message sent successfully');
-    } else {
-        alert('Error sending message, reason: ', apiResult.error);
-    }
+    jivo_api.setCustomData([
+      {"title": "Имя","content": "Контактное лицо"},
+      {"title": "Телефоон","content": "Конт. номер телефона"},
+      {"title": "Адрес","content": "Адрес доставки"},
+      {"title": "Комментарий","content": "Комментарий"},
+      {"title": "Общая сумма заказа","content": "333333"}
+   ]);
   }
 };
