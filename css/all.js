b859701369f0a06dc5c4e73df35e8c7b01a5b69d
shopceramic <75972538+shopceramic@users.noreply.github.com>
2020-12-17 02:58:07 +0300

    Add files via upload

---
+++ css/all.js
@@ -0,0 +1,13 @@
+home.style.marginTop = document.querySelector('header').getBoundingClientRect().bottom
+document.querySelectorAll('footer a').forEach(el => el.classList.add('d-block','m-3','text-light'));
+window.addEventListener('load', ()=>{
+// (()=>{
+//     (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(57523870, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true });
+//     let s = document.createElement('script');
+//     s.type = 'text/javascript';
+//     s.async = true;
+//     s.src = '//code-ya.jivosite.com/widget/tBP8rFP2fb';
+//     document.head.appendChild(s);
+// document.querySelectorAll('.bid').forEach(el => el.onclick =()=> jivo_api.open() );
+// })();
+});
